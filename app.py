9afa5f8812be9f44f1b114afef77891e4e6beae3
Thomas Tarantino <tommy1497@outlook.it>
2026-02-03 12:15:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 from whoscored.whoscored_events_data import load_whoscored_events_data
+import subprocess
+import sys
 
+# Installa la libreria da GitHub solo se non è già presente
+try:
+    from whoscored.whoscored_events_data import load_whoscored_events_data
+except ImportError:
+    subprocess.check_call([
+        sys.executable, "-m", "pip", "install",
+        "git+https://github.com/sahil-gidwani/football-data-webscraping.git#subdirectory=whoscored"
+    ])
+    from whoscored.whoscored_events_data import load_whoscored_events_data
 st.title("WhoScored Events")
 
 url = st.text_input("Inserisci URL partita WhoScored", "...")
